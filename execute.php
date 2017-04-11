0d2ad28a770aeac6e6ed82841ffbf39e3aa3b470
blackSource <blackSOurce@windowslive.com>
2017-04-11 23:03:40 +0700

    Added sql code

---
+++ execute.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bayu
+ * Date: 4/11/2017 AD
+ * Time: 10:23 PM
+ */
+
+//    $code = "INSERT INTO `seat`(`seatid`, `sid`, `max_seat`, `status`) VALUES ([seatid],[studentid],[max_seat],[status]) ";
+
+
+//        $servername = "188.166.248.254";
+//    $username = "blacksource_root"; // database id
+//    $password = "ifyounot"; // database password
+//    $dbname = "blacksource_bksys"; //database name
+//    $conn = new mysqli($servername,$username,$password,$dbname);
+//
+//        $code = "INSERT INTO `seat`(`seatid`, `sid`, `max_seat`, `status`) VALUES ('$i','',32,0) ";
+//        $conn->query($code);
