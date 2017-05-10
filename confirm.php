fc8728ced6f927e47df73a78705ef0bddcf6651a
Ayuth Mangmesap <ayuth.mang@gmail.com>
2017-05-10 23:57:47 +0700

    Finished with simple confirmed seat and revoke confirmed seat

--- confirm.php
+++ confirm.php
@@ -1,21 +1,80 @@
 <?php
 
-print_r ($_REQUEST);
+	ini_set('session.save_path', realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/../session'));
 
-foreach ($_GET as $name => $value) {
-    echo $name . ' : ' . $value . '<br />';
-}
-//echo "<table>";
-//
-//    foreach ($_POST as $key => $value) {	
-//        echo "<tr>";
-//        echo "<td>";
-//        echo $key;
-//        echo "</td>";
-//        echo "<td>";
-//        echo $value;
-//        echo "</td>";
-//        echo "</tr>";
-//    }
-//
-//echo "</table>";
+    session_start();
+    // $queryCheckIfStudentInCurrentSeat = "
+    //                   SELECT seat.seatid , seat.sid
+    //                   FROM seat
+    //                   WHERE seat.sid = '".$_SESSION['sid']."' ";
+	print_r ($_REQUEST);
+
+    $servername = "188.166.248.254";
+    $username = "blacksource_root"; // database id
+    $password = "ifyounot"; // database password
+    $dbname = "blacksource_bksys"; //database name
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+
+    }
+
+
+$selectedSeatId = $_REQUEST['selectedseatid'];
+
+
+
+	$sql_CheckIfSeatIsOwnAndConfirmedByCurrentStudentId = 
+	"
+	SELECT seat.sid
+	FROM seat
+	WHERE seat.sid = '".$_SESSION['sid']."' and seat.status=2
+	";
+
+	$objResult = $conn->query($sql_CheckIfSeatIsOwnAndConfirmedByCurrentStudentId);
+	$data = $objResult->fetch_assoc();
+
+
+	if(isset($data) && $data['sid'] === $_SESSION['sid']){
+		// Seat is own my me an confirmed 
+		// So we gonna remove this seat
+
+		print "I'm here";
+		$sql_RemoveCurrentSeatFromDTB = 
+		"
+		UPDATE seat
+		SET seat.sid = NULL , seat.status = 0 
+		WHERE seat.seatid = $selectedSeatId and seat.sid = '".$_SESSION['sid']."'
+		";
+
+		$queryResult = $conn->query($sql_RemoveCurrentSeatFromDTB);
+
+
+		// var_dump($queryResult);
+
+	}
+	else{
+		// Seat that available
+		// So we gonna reserve this seat
+
+		$sqlConfirmSeat = 
+		"
+		UPDATE seat
+		SET seat.status = 2
+		WHERE seat.seatid = $selectedSeatId and seat.sid = '".$_SESSION['sid']."'
+		";
+
+		$queryResult = $conn->query($sqlConfirmSeat);
+		if($queryResult)
+			print "Reserved seat $selectedSeatId successful" ;
+		else
+			print "Reserve seat $selectedSeatId failed";
+		// var_dump($queryResult);
+
+	}
+
+
+
+
+
+?>
